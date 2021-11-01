322a709fa41ec87cd7c046a004f06f8c0bd9ae34
davidfu <david.fu.df@gmail.com>
2021-11-01 19:12:14 -0400

    Initial Commit

---
+++ CSharp_WebServiceApplication/WebService1.asmx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using Newtonsoft.Json;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CSharp_WebServiceApplication
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        DBAccess objDBAccess = new DBAccess();
+        DataTable dtUsers = new DataTable();
+        DataTable dtCountries = new DataTable();
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        [WebMethod]
+        public string Countries()
+        {
+            dtCountries.Columns.Add("CountryName");
+            dtCountries.Columns.Add("Continent");
+
+            dtCountries.Rows.Add("Pakistan", "Asia");
+            dtCountries.Rows.Add("India", "Asia");
+            dtCountries.Rows.Add("Germany", "Europe");
+            dtCountries.Rows.Add("SriLanka", "Asia");
+            dtCountries.Rows.Add("Australia", "Australia");
+            dtCountries.Rows.Add("New Zealand", "AsAustraliaia");
+
+            return JsonConvert.SerializeObject(dtCountries);
+
+        }
+
+        [WebMethod]
+        public string dataTableForUsers(string id)
+        {
+            string query = $"Select * From Users Where ID = '{id}'";
+            objDBAccess.readDatathroughAdapter(query, dtUsers);
+
+            string result = JsonConvert.SerializeObject(dtUsers);
+            return result;
+
+
+        }
+    }
+}
---
+++ MyClientApp/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace MyClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 113);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(677, 293);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxID.Location = new System.Drawing.Point(51, 25);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(582, 29);
+            this.textBoxID.TabIndex = 1;
+            this.textBoxID.TextChanged += new System.EventHandler(this.textBoxID_TextChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(656, 25);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(83, 29);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
